376c61c86063aa095752e414f65a8c57434e7f92
Auston Stewart <auston@shedbuilt.net>
2018-04-15 11:54:33 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ sed -i '/components.png \\/{n;d}' doc/Makefile.in && \
 ./configure --prefix=/usr \
             --disable-static \
             --enable-docs && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install && \
-install -v -m644 doc/Vorbis* "${SHED_FAKEROOT}/usr/share/doc/libvorbis-1.3.5"
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install && \
+install -v -m644 doc/Vorbis* "${SHED_FAKE_ROOT}/usr/share/doc/libvorbis-1.3.5"
